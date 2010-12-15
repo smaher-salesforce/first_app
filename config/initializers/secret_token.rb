ae2f921202d6201ec2795029e1995a0f2b1c2181
Steve Maher <smaher@salesforce.com>
2010-12-15 15:54:31 -0800

    Initial commit with new Readme

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FirstApp::Application.config.secret_token = 'f7011bb9981a07aedb81a60ade9f454dff5772d9b28562e291fae9ae508ba79f3773e6b6da69ee457beb46b6974ed55695d1875bc365430e53fc9be65d7eec08'
